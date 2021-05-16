bf2508c396b86044cba1664bf8df9a01ce2f3a60
Leonardo Silvio de Moura <leonardosimoura@gmail.com>
2021-05-16 11:27:20 -0300

    init

---
+++ Controllers/MeasurementController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace argo_rollouts_sample.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MeasurementController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string service)
+        {
+            return Ok(new
+            {
+                service,
+                ok = true,
+                successPercent = 0.97
+            });
+        }
+    }
+}
